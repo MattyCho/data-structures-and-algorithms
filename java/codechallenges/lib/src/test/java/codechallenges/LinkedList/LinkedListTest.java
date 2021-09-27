19ebe0481658de91c272bad4e693ed62bf758c2c
MattyCho <myc.chosen@gmail.com>
2021-09-27 15:25:54 -0700

    complete code challenge 6

--- java/codechallenges/lib/src/test/java/codechallenges/LinkedList/LinkedListTest.java
+++ java/codechallenges/lib/src/test/java/codechallenges/LinkedList/LinkedListTest.java
@@ -15,9 +15,22 @@ public class LinkedListTest {
     newLL.insert(0);
     newLL.insert(74);
 
+    System.out.println("toString test:");
     System.out.println(newLL.toString());
 
-    System.out.println(newLL.includes(3));
-    System.out.println(newLL.includes(33));
+    System.out.println("List includes 3: " + newLL.includes(3));
+    System.out.println("List includes 33: " + newLL.includes(33));
+
+    System.out.println("Add 100 to the end of the list: ");
+    newLL.append(100);
+    System.out.println(newLL.toString());
+
+    System.out.println("insert 15 after 2:");
+    newLL.insertAfter(2, 15);
+    System.out.println(newLL.toString());
+
+    System.out.println("insert 44 before 5:");
+    newLL.insertBefore(5, 44);
+    System.out.println(newLL.toString());
   }
 }
