a593c78567ac1d4d32671a824849cddd09fcd305
MattyCho <myc.chosen@gmail.com>
2021-10-26 23:11:10 -0700

    started code challenge 27

---
+++ java/codechallenges/lib/src/main/java/codechallenges/cc27/MergeSort.java
@@ -0,0 +1,58 @@
+package codechallenges.cc27;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+  public static int[] mergeSort(int[] arr) {
+    int n = arr.length;
+
+    if (n > 1) {
+      int mid = n/2;
+      int[] left = new int[n-mid];
+      for (int i = 0; i < left.length; i++) {
+        left[i] = arr[i];
+      }
+      int[] right = new int[mid];
+      for (int i = 0; i < right.length; i++) {
+        right[i] = arr[mid+i];
+      }
+      mergeSort(left);
+      mergeSort(right);
+      merge(left, right, arr);
+    }
+    return arr;
+  }
+
+  public static int[] merge(int[] left, int[] right, int[] arr){
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < left.length && j < right.length) {
+      if (left[i] <= right[j]) {
+        arr[k] = left[i];
+        i++;
+        System.out.println("Left" + Arrays.toString(left));
+      } else {
+        arr[k] = right[j];
+        j++;
+        System.out.println("Right" + Arrays.toString(right));
+      }
+      k++;
+    }
+
+    if (i == left.length) {
+      for (;j < right.length; j++) {
+        arr[k] = arr[j];
+        k++;
+      }
+    } else {
+      for (;i < left.length; i++) {
+        arr[k] = arr[i];
+        k++;
+      }
+    }
+    return arr;
+  }
+}
